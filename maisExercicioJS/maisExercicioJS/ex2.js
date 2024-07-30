c50dfa13c9a652c56be80d44e32c1bf382c2becf
HannaEpelboim <h4nn42005@gmail.com>
2024-07-30 12:41:35 -0300

    Delete maisExercicioJS/maisExercicioJS directory

--- maisExercicioJS/maisExercicioJS/ex2.js
+++
@@ -1,8 +0,0 @@
-let num = parseInt(prompt("Digite um número inteiro: "));
-
-if(num%2 == 0){
-    alert(`${num} é par`);
-}
-else{
-    alert(`${num} é ímpar`);
-}
--- maisExercicioJS/maisExercicioJS/ex5.js
+++
@@ -1,8 +0,0 @@
-let compras = [];
-
-for(let i = 0; i<5; i++){
-    let item = prompt("Digite o item:");
-    compras.push(item);
-}
-
-alert(compras);
